9e0a216dab30063d772ea3fae19d9c1961c3dc2f
Dreamcrushers-sj <wangchaoiq@gmail.com>
2020-11-15 23:41:45 -0800

    first commit

---
+++ firebase signup and login/app.js
@@ -0,0 +1,48 @@
+let signup = () =>{
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+    
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+     .then((result)=>{
+         console.log(result)
+     })
+    
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+          console.log(errorMessage)
+      })
+      
+      
+    
+    
+}
+    
+    
+let login = () =>{
+    let email = document.getElementById("login-email")
+    let password = document.getElementById("login-password")
+
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    .then ((result)=> {
+        console.log("user login successfully")
+        console.log(result)
+    })
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage)
+      })
+      
+      
+}
+
+
+
+   
+
+
